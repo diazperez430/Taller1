73f53e72c9564e23c152f1c9ba21c5f882ab6408
Claudio Diaz <diazperez430@gmail.com>
2023-09-19 18:15:28 -0500

    [+] primera sección del taller realizada

---
+++ varyconst.js
@@ -0,0 +1,70 @@
+//* 1. Convertir la temperatura en Celsius ingresada por el ususario a Farenheit //*
+
+// temCelsius = prompt("Ingrese la temperatura en °C  y te la convierto en °F")
+// let calculo = temCelsius*1.8 + 32
+// temFarenheit = alert (`Su temperatura en °F es ${calculo}`)
+// console.log(calculo)
+
+
+
+//* 2. Pide al usuario que ingrese el radio de un círculo y calcula su área. *//
+
+// const PI = 3.14159
+// radio = prompt("Ingrese el radio del círculo en m y te calculo su área en m^2")
+// let area = Math.round(PI*Math.pow(radio, 2));
+// let resultado = alert(`El área del círculo es ${area} m^2`)
+
+
+
+/* 3. Afirmar si es mayor o menor de edad */
+
+// let dato = prompt("Ingrese su edad")
+// edad= Number(dato)
+
+// if (edad >= 18){
+//     alert("Usted es mayor de edad")
+// }
+// else if (edad >= 0  && edad <18){
+//     alert("Usted es menor de edad")
+// }
+// else {
+//     prompt ("Ingrese su edad")
+// }
+
+/* 4. Usa el prompt para tomar dos números como argumentos y utilice una variable para
+ almacenar la suma de estos números. Luego, muestra el resultado en la consola */
+
+//  let bienvenida = alert("Hola, hoy haremos una suma")
+//  let primerVariable = prompt("Ingresa el primer valor")
+//  let segundaVariable = prompt("Ingresa el segundo valor")
+//  let resultado = alert(`La suma de tus valores es
+//   ${Number(primerVariable) + Number(segundaVariable)}`)
+// let valorConsola = Number(primerVariable)+ Number(segundaVariable)
+// console.log(valorConsola)
+
+/* 5. Declara un tipo string con una cadena de texto larga. Luego, utiliza métodos de 
+cadenas para obtener la longitud de la cadena, convertirla a minúscula y mostrar la última
+palabra en mayúsculas */
+
+// let mayeutica = "Sólo sé que nada sé"
+
+// console.log(mayeutica.length)
+// let lastLetter = mayeutica.toUpperCase().slice(18)
+// let firstSection = mayeutica.toLowerCase().slice(0,18)
+// let combination = firstSection.concat(lastLetter)
+// console.log(combination)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
