6b8e3471b75cbfd65d70440e29751e9de4818aed
Claudio Diaz <diazperez430@gmail.com>
2023-09-19 20:50:16 -0500

    [+] Avance segundo item

---
+++ tiposDeDatos.js
@@ -0,0 +1,26 @@
+/*Declara una variable y asígnale un valor booleano. Luego, utiliza una estructura
+ condicional para determinar si el valor es verdadero o falso y muestra un mensaje 
+ apropiado, por ejemplo "Es verdadero" o "Es falso". */
+
+// let value = false
+// if (value==true){
+//     alert("Es falso")
+// }
+// else {
+//     alert("Es verdadero")
+// }
+
+/*Usa el prompt para que tome un número. Indique si el número es par o impar
+lanzando un alert() */
+
+
+// let numero = prompt("Ingrese cualquier número aquí y te diré si es par o impar")
+// let calculo = numero%2
+// if (calculo==0){
+//     alert('Su número es par')
+// }
+// else {
+//     alert("Su número es impar")
+// }
+
+
