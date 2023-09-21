59ca54c22ace5ef408ac4e024379d6322097406f
Claudio Diaz <diazperez430@gmail.com>
2023-09-21 16:23:33 -0500

    [+] Tercer punto iniciado

--- tiposDeDatos.js
+++ tiposDeDatos.js
@@ -1,4 +1,4 @@
-/*Declara una variable y asígnale un valor booleano. Luego, utiliza una estructura
+/* 1. Declara una variable y asígnale un valor booleano. Luego, utiliza una estructura
  condicional para determinar si el valor es verdadero o falso y muestra un mensaje 
  apropiado, por ejemplo "Es verdadero" o "Es falso". */
 
@@ -10,7 +10,7 @@
 //     alert("Es verdadero")
 // }
 
-/*Usa el prompt para que tome un número. Indique si el número es par o impar
+/* 2. Usa el prompt para que tome un número. Indique si el número es par o impar
 lanzando un alert() */
 
 
@@ -22,5 +22,42 @@ lanzando un alert() */
 // else {
 //     alert("Su número es impar")
 // }
+
+/* 3. Declara una lista que contenga diferentes tipos de datos (números, cadenas, boleanos, objetos, ect.)
+cuenta cuánntos valores contiene la lista e informa lanzando un alert. */
+
+// let lista = [2,"Hola mundo",false, {nombre:"pika",},5]
+// let cantidad = lista.length
+
+// alert(`Tu lista tiene ${cantidad} datos`)
+
+
+
+/* 4. Crea un programa que declare una lista de  nombres de frutas. Luego, elimina la primera fruta, ahora
+cambia el valor de la segunda frunta que ahora esta de primero en la lista, inserta una nueva fruta al final 
+de la lista y muestra toda la lista en consola */
+
+// let lista = ["Cherries", "Peaches", "Watermelons", "Coconuts", "Kiwi" ]
+
+// lista.shift(0)
+// lista[0]="Mangos"
+// lista.push("Apple")
+// console.log(lista)
+
+/* 5. Declara un objeto que represente auna persona con propiedades como: nombre, edad y dirección. Luego, 
+muestra la información de la persona en la consola accediendo directamente a sus propiedades. El mensaje debe 
+decir algo como: Juan tiene 45 años y vive en la calle 63 con carrera 24. */
+
+let persona = {
+    name : "Claudio",
+    age : 26,
+    address: "Carrera 36 #3-41",
+}
+
+console.log (`${persona.name} tiene ${persona.age} años y vive en la ${persona.address}`)
+
+
+
+
 
 
