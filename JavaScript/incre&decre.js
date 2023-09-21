cd43f72e048a9504e535318aec11a05bf85cadba
Claudio Diaz <diazperez430@gmail.com>
2023-09-21 17:51:42 -0500

    [+] 4 punto iniciado

---
+++ JavaScript/incre&decre.js
@@ -0,0 +1,40 @@
+/* 1. Declarar dos variables numéricas e incrementar una de ellas y decrementar la otra en una sola línea de código 
+utilizando operadores de incremento y decremento. Muestra los valores resultantes. */
+
+// let first = 5; first++; let second = 50; second--; console.log(first,second);
+
+
+
+/* 2. Usando el prompt, captura el nombre de un producto. Define una variable que se llame precio con el valor inicial
+de 0 (cero) e incrementa el precio en 1 por cada letra que tenga el producto, ejemplo: 
+
+1.1 Si el producto se llama Mango el precio debe ser de $5 la unidad. El mesaje será: El Mango tiene un valor de $5 la
+unidad.
+
+1.2 Si el producto se llama Mandarina, el precio debe ser de 9. El mensaje será: La mandarina tiene un valor de $ 9 la
+unidad*/
+
+
+// let producto = prompt("Ingresa aquí el nombre del producto y te diré su precio")
+
+// let precio = producto.length
+
+// alert(`El precio de su producto ${producto} es $${precio}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/operadores.js
@@ -0,0 +1,87 @@
+/* 1. Crea un programa que solicite al usuario dos números usando el prompt y realice operaciones de suma, resta,
+multipliación y división con estos números. Muestra los resultados en la consola para cada una de estas operaciones */
+
+// let primerNumero = prompt("Ingresa el primer número a operar")
+// let segundoNumero = prompt("Ingresa el segundo número a operar")
+
+// let times = primerNumero*segundoNumero
+// let add = Number(primerNumero) + Number(segundoNumero)
+// let substract = primerNumero - segundoNumero
+// let division = primerNumero/segundoNumero
+// console.log(times,add,substract,division)
+
+/* 2. Declara una variable numérica. Usa el prompt para pedir al usuario que ingrese un valor numérico. Utiliza
+operadores de comparación para determinar si son iguales o si una es mayor que la otra. Muestra los resultados en la
+consola*/
+
+// let firstVariable = 5;
+// let secondVariable = prompt("Ingresa un número");
+
+// console.log(
+    
+//     firstVariable == secondVariable,
+//     firstVariable < secondVariable,
+//     firstVariable > secondVariable,
+//     )
+
+/* 3. Usando el prompt solicita al usuario digitar su nombre y utilice el operador de concatenación (+) para unir
+el nombre ingresado de manera que el resultado se despliegue en un alert y sea el siguiente: Hola Juan, benvenido! */
+
+// let name = prompt("Ingresa aquí tu nombre")
+
+// alert ("Hola " + name + " bienvenid@")
+
+
+/* 4. Crea un programa que calcule el área de un triángulo utilizando la fórmula: area = (base*altura)/2. Solicita
+al usuario que ingrese los valores de la base y la altura usando el prompt.*/
+
+// let base = prompt("Ingresa aquí la base del triángulo en metros")
+// let altura = prompt("Ingresa aquí la altura del triángulo en metros")
+
+// let partialArea = Number(base)*Number(altura)
+// let trianguleArea= partialArea/2
+
+// alert(`El área del triángulo en metros cuadrados es igual a ${trianguleArea}`)
+
+
+/* 5. Declara una variable booleana llamada esMayor y asigna el resultado de una comparación entre dos edades
+(por ejemplo, 18 y 25). Luego, muestra el valor es mayor en la consola. */
+
+// let esMayor = false;
+
+// let Otto = 26
+// let Willy = 20
+
+// if (Otto == Willy){
+//     console.log("Tienen la misma edad")
+// }
+// else {console.log(esMayor)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
