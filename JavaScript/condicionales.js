e38ad61a63bd2779af6b1da21f00d800a52cfcf6
diazperez430 <diazperez430@gmail.com>
2023-09-26 18:58:32 -0500

    [+] changes added.

---
+++ JavaScript/condicionales.js
@@ -0,0 +1,17 @@
+/* 1. Crea un programa que solicite al usuario un número y determine si es positivo, negativo 
+o cero utilizando una estructura condicional (if). */
+
+// let number = prompt("Ingresa aquí un número")
+// number = Number(number)
+
+
+// if (number>0){
+//     alert("Su número es Positivo")
+// }
+// else if (number<0){
+//     alert("Su número es negativo")
+// }
+// else {
+//     alert("Su número es 0")
+// }
+
--- JavaScript/incre&decre.js
+++ JavaScript/incre&decre.js
@@ -21,7 +21,30 @@ unidad*/
 
 // alert(`El precio de su producto ${producto} es $${precio}`)
 
+/* 3. Es noche de amor y amistad, en esta ocasión el cover para ingresar a la disco incrementará
+ 2 veces para los chicos y decrementará 2 veces para las chicas. El valor habitual es de 7 
+ dólares. Usando el prompt solicite el nombre y el genero de una persona e imprima usando 
+ alert() el siguiente mensaje si es un chico Juan tu entrada a la disco vale $9 dólares, en
+  caso de ser chica Ana tu entrada a la disco vale $5 dólares */
 
+
+// let nombre = prompt("Ingresa tu nombre");
+// let genero = prompt("Ingresa tu género como (M / F)");
+// let precio = 7;
+
+// if(genero= "m" ){
+//     precio+=
+//     alert(`${nombre} tu entrada a la disco vale $${precio}`);
+//  }
+
+// else if(genero= "f" ){
+//     precio-=
+//     alert(`${nombre} tu entrada a la disco vale $${precio}`);
+// }
+
+// else{
+//     alert("No ingresaste un sexo válido M o F")
+// }
 
 
 
